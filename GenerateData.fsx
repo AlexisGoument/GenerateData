60c56abc19da4a5110e6567b4f92f1a89ebabfc0
Alexis <alexis.nguyen-goument@primeum.com>
2023-08-25 15:57:01 +0200

    add column RowId + remove debug messages

--- GenerateData.fsx
+++ GenerateData.fsx
@@ -32,41 +32,45 @@ let GetCities (offset) (limit: int) : GeoNameApi.Result[] =
 
 let random = new Random()
 
-let WriteModels filename (city: GeoNameApi.Result) =
+let WriteModels filename writedRows (city: GeoNameApi.Result) =
     use writer = new StreamWriter(filename, true)
     models
-    |> Array.iter (fun model -> 
-        sprintf "%s,%s,%s,%d,%d" city.Country city.Name model (random.Next(600,1000)) (random.Next(900, 1200))
+    |> Array.indexed
+    |> Array.iter (fun (i, model) ->
+        let rowIndex = i + writedRows
+        sprintf "%d;%s;%s;%s;%d;%d" rowIndex city.Country city.Name model (random.Next(600,1000)) (random.Next(900, 1200))
         |> writer.WriteLine)
-    ()
+    writedRows + models.Length
 
-let WriteCities filename (cities: GeoNameApi.Result[]) =
+let WriteCities filename writedRows (cities: GeoNameApi.Result[]) =
     cities
-    |> Array.iter (fun city -> WriteModels filename city)
+    |> Array.indexed
+    |> Array.map (fun (index, city) -> WriteModels filename ((index * nbModels) + writedRows) city)
+    |> Array.max
 
 let InitFile filename = 
     use writer = new StreamWriter(filename, false)
-    writer.WriteLine "Region,Secteur,Produit,Actuel,Objectif"
+    writer.WriteLine "RowId;Region;Secteur;Produit;Actuel;Objectif"
 
-let rec FillFile expectedNbCities filename writedCities = 
+let rec FillFile expectedNbCities filename (writedCities, writedRows) = 
     if (writedCities < expectedNbCities) then
         let nbMissingCities = expectedNbCities - writedCities
         let cities = 
             match nbMissingCities < 100 with
-            | true -> 
-                printfn "GetCities %d %d" writedCities nbMissingCities
-                GetCities writedCities nbMissingCities
-            | false -> 
-                printfn "GetCities %d 100" writedCities
-                GetCities writedCities 100
-        cities
-        |> WriteCities filename
-        writedCities + cities.Length
+            | true -> GetCities writedCities nbMissingCities
+            | false -> GetCities writedCities 100
+        
+        let writedRows =
+            cities
+            |> WriteCities filename writedRows
+
+        (writedCities + cities.Length, writedRows)
         |> FillFile expectedNbCities filename
+
 
 let GenerateFile expectedNbRows filename = 
     InitFile filename
     let expectedNbCities = expectedNbRows / nbModels
-    FillFile expectedNbCities filename 0
+    FillFile expectedNbCities filename (0, 0)
 
-GenerateFile 1000 "renaultRsO_1000.csv"
+GenerateFile 100000 "MeasureMassData\\renaultRsO_100000.csv"
